0454b9578e621922ec5ddf301867700ef486e60a
Maxim Radionov <maxim_radionov@bk.ru>
2023-09-05 19:05:51 +0300

    feat: завершает главную страницу

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,11 @@
 import {initHeaderMenu} from './modules/init-header-menu.js';
 import {initGuideSlider} from './modules/init-guide-slider.js';
+import {initPopularToursSlider} from './modules/init-popular-tours-slider.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('load', () => {
     initHeaderMenu();
     initGuideSlider();
+    initPopularToursSlider();
   });
 });
--- source/js/modules/init-guide-slider.js
+++ source/js/modules/init-guide-slider.js
@@ -1,19 +1,45 @@
 import {Swiper} from '../vendor/swiper-bundle.js';
-const initGuideSlider = () => {
-    const slider = document.querySelector('[data-slider="guide"]');
 
+const slider = document.querySelector('[data-slider="guide"]');
+const totalSlides = slider.querySelectorAll('.swiper-slide').length;
+
+const controls = document.querySelector('[data-slider-controls="guide"]');
+const prev = controls.querySelector('[data-slider-control="prev"]');
+const next = controls.querySelector('[data-slider-control="next"]');
+const fraction = controls.querySelector('[data-slider-fraction]');
+
+const breakpointMediaDesktop = window.matchMedia('(min-width: 1600px)');
+const breakpointMediaNotebook = window.matchMedia('(min-width: 1200px)');
+const breakpointMediaTablet = window.matchMedia('(min-width: 768px)');
+const breakpointMediaMobile = window.matchMedia('(min-width: 320px)');
+
+let visibleSlides;
+
+const breakpointChecker = () => {
+  if (breakpointMediaDesktop.matches) {
+    visibleSlides = 4;
+  } else if (breakpointMediaNotebook.matches) {
+    visibleSlides = 3;
+  } else if (breakpointMediaTablet.matches) {
+    visibleSlides = 2;
+  } else if (breakpointMediaMobile.matches) {
+    visibleSlides = 1;
+  }
+
+  fraction.textContent = `1 / ${totalSlides - visibleSlides + 1}`;
+
+  breakpointMediaDesktop.addListener(breakpointChecker);
+  breakpointMediaNotebook.addListener(breakpointChecker);
+  breakpointMediaTablet.addListener(breakpointChecker);
+  breakpointMediaMobile.addListener(breakpointChecker);
+};
+
+const initGuideSlider = () => {
     if (!slider) {
       return;
     }
 
-    const slideCount = slider.querySelectorAll('.swiper-slide').length;
-
-    const controls = document.querySelector('[data-slider-controls="guide"]');
-    const prev = controls.querySelector('[data-slider-control="prev"]');
-    const next = controls.querySelector('[data-slider-control="next"]');
-    const fraction = controls.querySelector('[data-slider-fraction]');
-
-    fraction.textContent = `1 / ${slideCount}`;
+    breakpointChecker();
 
     const swiper = new Swiper(slider, {
       slidesPerView: 4,
@@ -26,9 +52,27 @@ const initGuideSlider = () => {
         el: '.swiper-pagination',
         clickable: true,
       },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 40,
+        },
+        1600: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+      },
       on: {
         slideChange: () => {
-          fraction.textContent = `${swiper.realIndex + 1} / ${slideCount}`;
+          fraction.textContent = `${swiper.realIndex + 1} / ${totalSlides - visibleSlides + 1}`;
         }
       }
     });
